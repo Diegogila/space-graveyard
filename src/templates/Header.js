a6dd96d3fd1dc873c22d76b759da4aade1e25b69
diegogila <dgg26496@hotmail.com>
2022-07-17 17:03:23 -0500

    Se agrega toda la configuracion de webpack, se integran Page y Templates asi como la integracion de Babel

---
+++ src/templates/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    const view = `
+    <div class="header-main">
+        <div class="header-logo">
+            <h1>
+                <a href="/">
+                    Rick and Morty | Cementary
+                </a>
+            </h1>
+        </div>
+        <div class="header-nav">
+            <a herf="#/about/">
+                About
+            </a>
+        </div>
+    </div>`;
+    return view;
+};
+
+export default Header;
